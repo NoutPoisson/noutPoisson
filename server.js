a790080237cd29fa34dd3a4f23bb05454eb7b3f7
Emmanuel Narcisse <emmanuel.narcisse@emadis-shop.fr>
2018-08-26 19:48:10 +0400

    SSL config

--- server.js
+++ server.js
@@ -16,11 +16,25 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/ang-node/index.html'))
 });
 
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
 
 
 
 
-const port = process.env.PORT || 4600;
+const port = process.env.PORT || 8080;
 
 app.listen(port, (req, res) => {
     console.log('RUNNING on port' + " " + port);
